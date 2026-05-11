0df0395ecc2c893bd19dd5c8794663af5ed9edda
Rafael Moreira <rafaelmrjgit@outlook.com>
2026-05-11 17:19:45 -0300

    Basic layout ideas
    - Dynamic layouts using Flexbox.
    - Popups featuring synchronization between Slider and EditText in the project specifications section.
    - Cards with button animations and a visual scaling effect.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+    //adicionadas
+    implementation("com.google.android.flexbox:flexbox:3.0.0")
 }
